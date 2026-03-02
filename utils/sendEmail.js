59caba203f27515a2e2fbed6cae1abcae5262d86
naadiya sakkeer <naadiyasakkeer@gmail.com>
2026-03-02 15:29:50 +0530

    gmail issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,11 +1,18 @@
 // utils/sendEmail.js
 const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Gmail App Password
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
